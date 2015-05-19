83c222461476671234d70bdd5556b43506716e1f
nthiep <hiep.iuh@gmail.com>
2015-05-19 17:41:58 +0700

    update gateway

--- gshproject/node/utils.py
+++ gshproject/node/utils.py
@@ -4,8 +4,8 @@ from django.contrib.gis.utils import GeoIP
 def create_gateway(sender, instance=None, created=False, **kwargs):
 	""" create root domain of user """
 	if created:
-    geo = GeoIP()
-    data = geo.city(instance.ip)
+	geo = GeoIP()
+	data = geo.city(instance.ip)
 		instance.city = data['city']
-    instance.country = data['country_name']
-    instance.save()
+	instance.country = data['country_name']
+	instance.save()
